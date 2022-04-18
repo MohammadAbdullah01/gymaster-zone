44922ebc874091891093af3ca46881e251f23fd7
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-04-18 20:33:56 +0600

    environment variable set

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBrkIaR2Sws-RRMWsxo0JSP-tRZAgXjKE4",
-    authDomain: "gymaster-78fd8.firebaseapp.com",
-    projectId: "gymaster-78fd8",
-    storageBucket: "gymaster-78fd8.appspot.com",
-    messagingSenderId: "341418030855",
-    appId: "1:341418030855:web:90d444d3ece524294337a2"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
