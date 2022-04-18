a44da16df722fab1025fef5834b856da07120f99
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-04-18 22:23:35 +0600

    code cleaned

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -8,7 +8,7 @@ const Services = () => {
     const { services } = useServices()
     return (
         <div className='service'>
-            <h1 className='text-center py-5 services-title'>HAVE A LOOK AT OUR SERVICES </h1>
+            <h1 className='text-center py-5 services-title'>HAVE A LOOK AT MY SERVICES </h1>
             <Row className='mx-auto'>
                 {services?.map(service => <Service
                     key={service.id}
