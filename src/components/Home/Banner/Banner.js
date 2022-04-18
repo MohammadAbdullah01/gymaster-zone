796a0e4e6c6299593761f1d4796d5d76af24fe17
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-04-18 21:55:19 +0600

    banner sec modified

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,44 +1,38 @@
 import React, { useEffect } from 'react';
-import { Carousel } from 'react-bootstrap';
-import banner1 from '../../../images/banner/banner1.jpg'
-import banner2 from '../../../images/banner/banner2.jpg'
-import banner3 from '../../../images/banner/banner3.jpg'
+import { Col, Container, Row } from 'react-bootstrap';
 import './Banner.css'
-
+import banner from '../../../images/banner/banner.png'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Banner = () => {
-
+    useEffect(() => {
+        Aos.init()
+    }, [])
     return (
-        <Carousel className='banner'>
-            <Carousel.Item interval={3000}>
-                <img
-                    className="banner-img d-block w-100"
-                    src={banner1}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={3000}>
-                <img
-                    className="banner-img d-block w-100"
-                    src={banner2}
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={3000}>
-                <img
-                    className="banner-img d-block w-100"
-                    src={banner3}
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel >
+        <div className='banner-container'>
+            <Container>
+                <Row>
+                    <Col sm={12} md={12} lg={6}>
+                        <div>
+                            <img className='banner-img' src={banner} alt="" />
+                        </div>
+                    </Col>
+                    <Col sm={12} md={12} lg={6}>
+                        <div className='banner-title-container'>
+                            <div className='quotee-container'>
+                                <div data-aos="fade-right">
+                                    <h1 className='quotee'><span className='stroked'>PERFECT</span> SHAPE</h1>
+                                </div>
+                                <div data-aos="fade-left">
+                                    <h1 className='quotee'>PERFECT <span className='stroked'>LIFE</span></h1>
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };
 
